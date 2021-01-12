b557ff0ee190bed5a08295833d0429c84336c472
Nytelife26 <xtylerjrx@gmail.com>
2021-01-12 22:59:03 +0000

    feat: debug output

--- gitcord-release-changelogger.sh
+++ gitcord-release-changelogger.sh
@@ -3,13 +3,15 @@ BODY=$(echo "$RELEASE_BODY" | sed -r \
 	-e 'N' \
 	-e '$!ba' \
 	-e 's/\n/\t\t/g' \
+	-e 's/\r//g' \
 	-e 's/\t\t\t\t/\t\t/g' \
 	-e 's/- +//g' \
 	-e 's/### ([^\t]+)\t\t/"}, {"name": "\1", "value": "/g' \
 	-e 's/^"}, //' \
 	-e 's/$/"}/g' \
 	-e 's/\t\t/\\n/g')
-PAYLOAD="{\"username\": \"GitHub Release\", \"embeds\": [{\"title\": \"$RELEASE_NAME\", \"fields\": [$BODY]}]}"
+#PAYLOAD="{\"username\": \"GitHub Release\", \"embeds\": [{\"title\": \"$RELEASE_NAME\", \"fields\": [$BODY]}]}"
+PAYLOAD="{\"username\": \"GitHub Release\", \"content\": \"${BODY@Q}\"}"
 echo "$PAYLOAD"
 echo ::set-output name=response::$(curl -X POST \
      -H "Content-Type: application/json" \
