8d9f5fd0714e754d31247768f69e3afbbfdf6d16
Nytelife26 <xtylerjrx@gmail.com>
2021-01-14 23:30:27 +0000

    fix: resolve bash formatting disparity

--- gitcord-release-changelogger.sh
+++ gitcord-release-changelogger.sh
@@ -1,3 +1,4 @@
+set -e
 BODY=$(echo "$RELEASE_BODY" | sed -r \
 	-e ':a' \
 	-e 'N' \
@@ -11,7 +12,7 @@ BODY=$(echo "$RELEASE_BODY" | sed -r \
 	-e 's/^"}, //g' \
 	-e 's/$/"}/g' \
 	-e 's/\t\t/\\n/g' \
-	-e 's/\{"name": "(.+)", "value": "\\n"\}/\{"name": "Meta", "value": "\1"\}/' \
-	-e 's/\{"name": ".+", "value": "\\n"\}, //g')
+	-e 's/\{"name": "(.+)", "value": " *\\n *"\}/\{"name": "Meta", "value": "\1"\}/' \
+	-e 's/\{"name": ".+", "value": " *\\n *"\}, //g')
 PAYLOAD="{\"username\": \"GitHub Release\", \"avatar_url\": \"https://github.com/kludge-cs/gitcord-release-changelogger/raw/master/GitHub-Mark-120px-plus.png\", \"embeds\": [{\"title\": \"$RELEASE_NAME\", \"fields\": [$BODY]}]}"
 echo ::set-output name=payload::$PAYLOAD
