23995748b05b428f39451b49905d19e27dccc7df
Nytelife26 <xtylerjrx@gmail.com>
2021-01-14 00:51:53 +0000

    feat: support header lines according to spec

--- gitcord-release-changelogger.sh
+++ gitcord-release-changelogger.sh
@@ -6,12 +6,15 @@ BODY=$(echo "$RELEASE_BODY" | sed -r \
 	-e 's/\r//g' \
 	-e 's/\t\t\t\t/\t\t/g' \
 	-e 's/- +//g' \
-	-e 's/### ([^\t]+)\t\t/"}, {"name": "\1", "value": "/g' \
-	-e 's/^"}, //' \
+	-e 's/"/\"/g' \
+	-e 's/#{1,3} ([^\t]+)\t\t/"}, {"name": "\1", "value": "/g' \
+	-e 's/^"}, //g' \
 	-e 's/$/"}/g' \
-	-e 's/\t\t/\\n/g')
-PAYLOAD="{\"username\": \"GitHub Release\", \"embeds\": [{\"title\": \"$RELEASE_NAME\", \"fields\": [$BODY]}]}"
-echo "$PAYLOAD"
+	-e 's/\t\t/\\n/g' \
+	-e 's/\{"name": "(.+)", "value": "\\n"\}/\{"name": "Meta", "value": "\1"\}/' \
+	-e 's/\{"name": ".+", "value": "\\n"\}, //g')
+PAYLOAD="{\"username\": \"GitHub Release\", \"avatar_url\": \"https://github.com/kludge-cs/gitcord-release-changelogger/raw/master/GitHub-Mark-120px-plus.png\", \"embeds\": [{\"title\": \"$RELEASE_NAME\", \"fields\": [$BODY]}]}"
+echo ::set-output name=payload::$PAYLOAD
 echo ::set-output name=response::$(curl -X POST \
      -H "Content-Type: application/json" \
 	 -d "$PAYLOAD" \
